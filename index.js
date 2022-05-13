8dd8b097c117966ed4e116e0590a4edb24435fa6
Eric Alvarez <alvarezaeric592@gmail.com>
2022-05-13 14:49:41 -0500

    añadi version desktop fase 1, añadi funciones mobiles

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ boton.addEventListener("click",()=>{
     menu.classList.toggle("nav--bar-items-show");
 
 })
+
+
+
+const nosotros = document.querySelector(".us");
+
+
+nosotros.addEventListener("click",()=>{
+
+    menu.classList.remove("nav--bar-items-show")
+
+})
+
+const contacto = document.querySelector(".contact")
+
+
+contacto.addEventListener("click",()=>{
+
+    menu.classList.remove("nav--bar-items-show")
+
+})
+
+
